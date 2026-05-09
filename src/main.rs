13f85252c47920aa289c5d4d68f2d3eea34c35bf
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-09 00:44:16 -0300

    Revert "Do not add a forward slash if the path begins with a forward slash"
    This reverts commit 77fb4270d1227c2883183e422ce9b52d39cfd726.

--- src/main.rs
+++ src/main.rs
@@ -43,9 +43,7 @@ fn makedirp(path: &str, mode: u32) -> Result<(), Error> {
     let splits: Vec<&str> = path.split("/").collect();
 
     for sub_str in splits {
-        if !path_str.starts_with("/") {
-            path_str.push('/');
-        }
+        path_str.push('/');
         path_str.push_str(sub_str);
 
         println!("Checking if {} exists", path_str);
