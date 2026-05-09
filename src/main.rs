f9bdf235b8f0073c86c54b8436ed46e270aee06a
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-09 00:37:25 -0300

    Propagate errors in exists

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,7 @@ fn makedirp(path: &str, mode: u32) -> Result<(), Error> {
         path_str.push('/');
         path_str.push_str(sub_str);
 
-        if !exists(&path_str).unwrap_or(false) {
+        if !exists(&path_str)? {
             println!("Creating directory {} with mode {}.", path_str, mode);
             makedir(&path_str, mode)?;
         }
