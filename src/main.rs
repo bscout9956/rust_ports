9698def866e09d8b3d1668d09cdec1a25fcb6f1b
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-09 19:01:56 -0300

    Fix n_flag and strlen logic
    Also rename variables

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,22 @@ fn main() -> io::Result<()> {
         n_flag = true;
     }
 
-    let mut p: String = String::new(); // p seems to be the buffer for the string
-    i = if n_flag { 1 + 1 } else { 1 + 0 };
-    while i < argv.len() {
-        p.push_str(argv.get(i).unwrap());
+    let mut echo_str: String = String::new();
+    let mut i: usize = if n_flag { 0 } else { 1 };
+    while i <= argv.len() {
+        echo_str.push_str(argv.get(i).unwrap());
         if i < argv.len() - 1 {
-            p.push_str(" ");
-            i+=1;
+            echo_str.push_str(" ");
+            i += 1;
         }
-        i+=1;
+        i += 1;
     }
 
     if n_flag == false {
-        p.push_str("\n");
+        echo_str.push_str("\n");
         // The C code would increment the pointer here but we don't really need to that as we won't read anything else...
     }
 
-    io::stdout().write_all(p.as_bytes())?;
+    io::stdout().write_all(echo_str.as_bytes())?;
     Ok(())
 }
