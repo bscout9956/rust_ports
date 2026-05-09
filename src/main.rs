dace320a02092dafb4e3152ed2bf02b431f98c24
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-09 19:18:11 -0300

    Rework the logic to be less like the og code

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,24 @@ use std::{
 };
 
 fn main() -> io::Result<()> {
-    let argv: Vec<String> = env::args().collect();
+    let argv: Vec<String> = env::args().skip(1).collect();
 
     let mut n_flag: bool = false;
     
-    if argv.len() > 1 && argv.get(0).is_some() && argv.get(0).unwrap() == "-n" {
+    if argv.len() > 0 && argv.get(0).is_some() && argv.get(0).unwrap() == "-n" {
         n_flag = true;
     }
 
     let mut echo_str: String = String::new();
-    let mut i: usize = if n_flag { 0 } else { 1 };
-    while i <= argv.len() {
-        echo_str.push_str(argv.get(i).unwrap());
-        if i < argv.len() - 1 {
-            echo_str.push_str(" ");
-            i += 1;
-        }
-        i += 1;
+    let start_index: usize = if n_flag { 1 } else { 0 };
+    
+    for str in &argv[start_index..] {
+        echo_str.push_str(str);
+        echo_str.push_str(" ");
     }
 
     if n_flag == false {
         echo_str.push_str("\n");
-        // The C code would increment the pointer here but we don't really need to that as we won't read anything else...
     }
 
     io::stdout().write_all(echo_str.as_bytes())?;
